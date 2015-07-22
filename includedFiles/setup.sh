fe671df574c668c0a38399175f40b9a7b271782c
Justin Dawson <jdawson@ku.edu>
2015-07-22 14:32:00 -0500

    fixed reboot scripts

--- includedFiles/setup.sh
+++ includedFiles/setup.sh
@@ -2,11 +2,23 @@
 
 cd ~/demo
 
-mv *.txt ..
+mv caPrivateKey.txt caPublicKey.txt ekpub.txt goldenPcrComposite.txt ..
+
+#Set up so modprobes get executed on reboot, and tpm emulator gets installed
+# if not already there
+
+rc_update=`grep xen_gntalloc /etc/rc.local`
+
+if [ "$rc_update" == "" ]; then
+
+     cat rcSegment >> /etc/rc.d/rc.local
+ 
+fi
 
 xen_tpmd=`modprobe xen_gntalloc 2>&1; modprobe xen_gntdev 2>&1; modprobe xen_evtchn; modprobe tpmd_dev 2>&1;`
 
 if [ "$xen_tpmd" != "" ];then
-   ./vmYumSetup.sh &
+   ./vmYumSetup.sh 2>&1 &
    disown
 fi
+
--- includedFiles/vmYumSetup.sh
+++ includedFiles/vmYumSetup.sh
@@ -12,20 +12,4 @@ mv xen-c6-tweaked.repo /etc/yum.repos.d
 rpm -i $EPEL_RPM_LINK
 yum -y install xen-devel-4.2.2 xen-runtime-4.2.2
 
-./emulatorSetup.sh
-
-echo "#!/bin/sh
-#
-# This script will be executed *after* all the other init scripts.
-# You can put your own initialization stuff in here if you don't
-# want to do the full Sys V style init stuff.
-
-touch /var/lock/subsys/local
-
-modprobe xen_gntalloc
-modprobe xen_gntdev
-modprobe xen_evtchn
-modprobe tpmd_dev
-" > /etc/rc.local
-
 reboot

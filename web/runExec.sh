1b774557975c3f068fadd5e9f8ac52eee4a228b1
Jason Gevargizian <jgevargi@ittc.ku.edu>
2015-08-11 13:27:57 -0500

    Screen session name

--- web/runExec.sh
+++ web/runExec.sh
@@ -7,7 +7,7 @@ att=$4
 ca=$5
 
 kill -9 `pidof shellinaboxd`
-shellinaboxd --disable-ssl -s /:root:root:HOME:"sshpass -p "armored" ssh -t root@$ip screen -r -S somethingelse"
+shellinaboxd --disable-ssl -s /:root:root:HOME:"sshpass -p "armored" ssh -t root@$ip screen -r -S screen_app"
 
 ssh root@$ip "~/demo/runExec.sh $executable $app $att $ca"
 
